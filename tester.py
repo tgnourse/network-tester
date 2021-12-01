1c4a658499f76cb48775dfa67c943937a5dbfb75
Thomas Nourse <tgnourse@gmail.com>
2021-11-30 22:44:48 -0800

    Update to be compatible with python 3.5.

--- tester.py
+++ tester.py
@@ -7,22 +7,10 @@ import argparse
 def upload_data(host, port, user, password, dbname, data):
     print('Uploading this data:\n')
     pprint.pprint(data)
-    print(f'{user}@{host}:{port}/{dbname}')
+    print('{}@{}:{}/{}'.format(user, host, port, dbname))
     client = InfluxDBClient(host, port, user, password, dbname, ssl=True, verify_ssl=False)
     return client.write_points(data)
 
-
-dns = {
-    'Cloudflare DNS' : '1.1.1.1',
-    'Google': '8.8.8.8',
-    'Level3 Communications DNS': '4.2.2.2',
-    'OpenDNS': '208.67.222.222'
-}
-
-gateways = {
-        'Local Gateway': '192.168.1.1',
-    'WAN Gateway': '198.27.186.210',
-}
 
 # Expecting a dict of name : ips
 def get_results_dict(destinations):
@@ -43,7 +31,6 @@ def transform_points(ip_results):
     points = []
     for ip, results in ip_results.items():
         for result in results:
-            print(ip, result.success, result.error_message, result.time_elapsed_ms)
             points.append(
                 {
                     'measurement': 'ping',
@@ -69,13 +56,9 @@ parser.add_argument('--influxdb_database')
 parser.add_argument('destinations', nargs='+')
 args = parser.parse_args()
 
-ip_results = get_results(args.destinations)
-
-pprint.pprint(transform_points(ip_results))
-
 upload_data(
     args.influxdb_host, args.influxdb_port, args.influxdb_user, args.influxdb_password, args.influxdb_database,
-    transform_points(ip_results))
+    transform_points(get_results(args.destinations)))
 
 # test_uploading(args.influxdb_host, args.influxdb_port, args.influxdb_user, args.influxdb_password, args.influxdb_database)
 # test_parsing()
