c59266158a26652bf9ccdaa7502f17349944ab0d
Thomas Nourse <tgnourse@gmail.com>
2022-01-16 16:01:48 -0800

    Add SSID, DNS/Gateway detection. Add lame duck mode for easier testing. Add requirements.txt for easy upgrades.

--- tester.py
+++ tester.py
@@ -2,14 +2,19 @@ from pythonping import ping
 import pprint
 from influxdb import InfluxDBClient
 import argparse
+import os
 
 # Upload to InfluxDB
-def upload_data(host, port, user, password, dbname, data):
+def upload_data(host, port, user, password, dbname, data, lame_duck=False):
     print('Uploading this data:\n')
     pprint.pprint(data)
     print('{}@{}:{}/{}'.format(user, host, port, dbname))
     client = InfluxDBClient(host, port, user, password, dbname, ssl=True, verify_ssl=False)
-    return client.write_points(data)
+    if not lame_duck:
+        return client.write_points(data)
+    else:
+        print('Lame duck mode NOT SENDING DATA')
+    return None
 
 
 def get_results(destinations):
@@ -48,20 +53,41 @@ parser.add_argument('--influxdb_port', type=int, default=8086)
 parser.add_argument('--influxdb_user')
 parser.add_argument('--influxdb_password')
 parser.add_argument('--influxdb_database')
-parser.add_argument('--device')  # every device should be unique
-parser.add_argument('--ssid')  # every device should be unique
-parser.add_argument('--device_area')  # up to anyone how to define this
+parser.add_argument('--device', help='Every device should be unique')
+parser.add_argument('--ssid', default=None, help='If none will try to discover it using "iwgetid"')
+parser.add_argument('--device_area', help='Up to anyone how to define this, represents grouping "above" device')
+parser.add_argument('--add_dns_destination', type=bool, default=False,
+                    help='adds the local DNS as a ping destination (usually ends up being the gateway)')
+parser.add_argument('--lame_duck', type=bool, default=False,
+                    help='Do not actually post the results to influxDB')
 parser.add_argument('destinations', nargs='+')
 args = parser.parse_args()
 
+if args.add_dns_destination:
+    import dns.resolver  # doing this here for backwards compatibility if not installed
+    args.destinations.append(dns.resolver.Resolver().nameservers[0])
+
+
+if args.ssid is None:
+    # Linux / Raspberry Pi
+    ssid = os.popen("iwgetid -r").read()
+    if len(ssid) > 0:
+        args.ssid = ssid
+    else:
+        # Try Mac / OSX
+        ssid = os.popen("/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I  | awk -F' SSID: '  '/ SSID: / {print $2}'").read().strip()
+        if len(ssid) > 0:
+            args.ssid = ssid
+
 upload_data(
     args.influxdb_host, args.influxdb_port, args.influxdb_user, args.influxdb_password, args.influxdb_database,
-    transform_points(get_results(args.destinations), args.device, args.ssid, args.device_area))
+    transform_points(get_results(args.destinations), args.device, args.ssid, args.device_area),
+    args.lame_duck)
 
 # Must haves
 # TODO: Cache if upload to Grafana fails
-# TODO: Gather connected SSID automatically
 
 # Nice to haves
+# TODO: Gather visible SSIDs automatically
 # TODO: Add SpeedTest integration, maybe this: https://github.com/sivel/speedtest-cli
 # TODO: Add traceroute integration, maybe this: https://github.com/hardikvasa/webb
